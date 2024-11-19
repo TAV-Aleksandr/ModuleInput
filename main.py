19e9cdb328c97ced438f4b77490373402294e381
TAV-Aleksandr <a.tyshik@mail.ru>
2024-11-19 21:55:12 +0700

    Вводный модуль. Практическое задание.

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+    # "1st program"
+print('9**0.5*5 =',9**0.5*5)
+
+    # "2nd program"
+print('(9.99>9.98 ang 1000!=1000.1) =', 9.99>9.98 and 1000!=1000.1)
+
+    # "3rd program"
+print('2*2+2 =', 2*2+2)
+print('2*(2+2) =', 2*(2+2))
+print('2*2+2 == 2*(2+2) =', 2*2+2 == 2*(2+2))
+
+    # "4th program"  '123.456' - cтрока"
+print("'123.456' - cтрока")
+print("int( float('123.456')*10 %10) =", int( float('123.456')*10 %10))
